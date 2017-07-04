b246b2681deb5386ee40fa38367c02ecfaa76abb
Spence Peacock <spencerpeacock@gmail.com>
2017-07-03 19:19:23 -0500

    javascript practice

---
+++ jsbasics2.js
@@ -0,0 +1,31 @@
+function magic(x,y){
+//    if(x.constructor == Array){
+//        for(i=0; i < x.length; i++){
+//            console.log(x[i]*y);
+//        }
+//        }
+     if(x.constructor == String){
+        console.log(x.repeat(y));
+    }
+    }
+
+//Test 1
+//let test1 = magic(5,2);
+//console.log(test1);
+//
+
+//Test2
+//let test2 = magic(0,0)
+//console.log(test2);
+
+//Test3
+//let test3 = magic([1, 2, 3], 2);
+//console.log(test3);
+
+//Test4
+//let test4 = magic(7, "three");
+//console.log(test4);
+
+//Test5
+//let test5 = magic("brendon ", 4);
+//console.log(test5);
