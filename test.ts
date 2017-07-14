8336d865b8f0f7a3517513960c8b302b83e161dd
Spence Peacock <spencerpeacock@gmail.com>
2017-07-14 12:40:04 -0500

    practice with typescript

---
+++ test.ts
@@ -0,0 +1,12 @@
+var myNum: number = 5;
+var myString: string = "hello universe";
+var arrayOfNumbers: number[1,2,3,4];
+
+var myObj: Object;
+
+let myObj ={
+name:'Bill'
+}
+let{'bill}=myObj'}
+
+let [true,false,true,true] = input;
