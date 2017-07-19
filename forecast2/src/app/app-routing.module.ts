36243f10da15b88a639f1c3071e1e182304c2376
Spence Peacock <spencerpeacock@gmail.com>
2017-07-19 00:15:01 -0500

    practice with api and routing

--- forecast2/src/app/app-routing.module.ts
+++ forecast2/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{DallasComponent} from './dallas/dallas.component';
+import{SeattleComponent} from './seattle/seattle.component';
+import{BurbankComponent} from './burbank/burbank.component';
+import{ChicagoComponent} from './chicago/chicago.component';
+import{DcComponent} from './dc/dc.component';
+import{SanjoseComponent} from './sanjose/sanjose.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+ {path:'dallas', component: DallasComponent},
+ {path: 'seattle', component: SeattleComponent},
+ {path: 'burbank', component: BurbankComponent},
+ {path: 'sanjose', component: SanjoseComponent},
+ {path: 'dc', component: DcComponent},
+ {path: 'chicago', component: ChicagoComponent},
 ];
 
 @NgModule({
